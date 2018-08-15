084cf8ab5f72317c13cdecf7e1a66498781f02d3
vngvbupt <lulifeng_v@163.com>
2018-08-15 17:18:35 +0800

    first commit

---
+++ public/mockdata/coupon.js
@@ -0,0 +1,26 @@
+const data = {
+    "counts":3,
+    "total_price":'316',
+    "data":[
+        {
+            "id":1,
+            "date":"6-27",
+            "origin":"泰国soft驱蚊水",
+            "price":56
+        },
+        {
+            "id":2,
+            "date":"6-27",
+            "origin":"赠送电子券",
+            "price":200
+        },
+        {
+            "id":3,
+            "date":"6-27",
+            "origin":"新疆天润酸奶",
+            "price":60
+        },
+    ]
+}
+
+export default data;
