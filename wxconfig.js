26a04d3be91b78605f7cf1555c866fff6bbefb51
Leo <Leowyyx@163.com>
2018-10-23 16:33:19 +0800

    47fenqutupian

--- wxconfig.js
+++ wxconfig.js
@@ -4,8 +4,8 @@ module.exports = {
   'encodingAESKey': 'PNuRoIzJBEWYCOR96Zrxe0h1bZCOACEkY9b0gQDeDms',
  
   
-  // 'secondaryHost': 'http://ymymmall.swczyc.com/ver',
-  'secondaryHost': 'http://ymymmall.lvxingbox.cn/ver',
+  'secondaryHost': 'http://ymymmall.swczyc.com/ver',
+  //'secondaryHost': 'http://ymymmall.lvxingbox.cn/ver',
 
   // 'appId': 'wx6d6fd71af24c22c3',
   // 'appSecret': '0094e566a880a093082b7e805f5b1c71',
@@ -14,5 +14,6 @@ module.exports = {
 
   'appId': 'wxc636210b469ce7dc',
   'appSecret': 'fc785c46f24105fa3a883e64a01fe2e9',
-  'domainHost': 'http://ymymmall.lvxingbox.cn',
+  // 'domainHost': 'http://ymymmall.lvxingbox.cn',
+  'domainHost': 'http://ymymmall.swczyc.com',
 };
